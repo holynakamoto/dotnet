c23737a6ed9bea543995855a018f58255795e907
Nick Moore <nickmoore@Nicks-MacBook-Pro.local>
2020-04-10 12:30:35 -0700

    update

--- gradebook/test/Gradebook.Tests/BookTests.cs
+++ gradebook/test/Gradebook.Tests/BookTests.cs
@@ -3,21 +3,18 @@ using Xunit;
 
 namespace Gradebook.Tests
 {
-    public class UnitTest1
+    public class BookTests
     {
         [Fact]
         public void Test1()
         {
             // arrange
-            var x = 5;
-            var y = 2;
-            var expected = 7;
+            var book = new Book("");
 
             // act
-            var actual = x * y;
 
             // assert
-            Assert.Equal(expected, actual);
+
         }
     }
 }
